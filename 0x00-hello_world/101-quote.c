c0163c472243c5e51cf2a1130994f2ec52f704af
Bright Otu <bionethe69@gmail.com>
2022-09-11 17:55:36 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-#include <stdlib.h>
-
-
+#include <unistd.h>
 
 /**
  *
- *  * main - Entry point
+ *  * main - Prints out the last part of a quote in the standard error.
  *
- *   * Return: Always 1 (Success)
+ *   * Return: 1 if success.
  *
  *    */
 
@@ -16,10 +14,10 @@ int main(void)
 
 {
 
-		write(STDOUT_FILENO, "and that piece of art is useful
-
-							\" - Dora Korpar, 2015-10-19\n", 59);
+		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 
 			return (1);
 
 }
+
+
