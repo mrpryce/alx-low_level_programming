7976d4620ead7c6dce0b07ffeb8f39d3b2923c2c
Bright Otu <bionethe69@gmail.com>
2022-09-21 15:06:33 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,24 @@
 #include "bright.h"
-/**
- * leet - encodes a string
- * @str: the string to encode
- *
- * Return: the encode string
- */
-char *leet(char *str)
-{
-	int i, j;
-	char c[] = "aAeEoOtTlL";
-	char n[] = "4433007711";
 
-	i = 0;
-	while (str[i] != '\0')
+/**
+ * leet - a function that encodes a string into 1337.
+ * @s: an input string to encode
+ * Return: An encode string
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	while (s[i])
 	{
-		j = 0;
-		while (c[j] != '\0')
-		{
-			if (str[i] == c[j])
-			{
-				str[i] = n[j];
-			}
-			j++;
-		}
+		for (j = 0; j < 10; j++)
+			if (s[i] == str[j])
+				s[i] = subs[j];
+
 		i++;
 	}
-	return (str);
+
+	return (s);
+}
