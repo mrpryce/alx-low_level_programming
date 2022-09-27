c1faa4bbeb5f0862a1b19bab9151eaf3766bc3dc
Bright Otu <bionethe69@gmail.com>
2022-09-26 22:53:15 -0700

    set-string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,12 @@
-#include "bright.h"
+#include "main.h"
 
 /**
- * set_string - sets value of a pointer to a char
- * @s: double pointer to a string
- * @to: pointer to the string to set s to
- * Return: Description of the returned value
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
  */
+
 void set_string(char **s, char *to)
 {
 	*s = to;
-}
+
