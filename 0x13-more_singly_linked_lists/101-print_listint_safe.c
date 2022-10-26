105c91275102cc9b2c7f9456211bf3a76534c724
Bright Otu <bionethe69@gmail.com>
2022-10-25 21:11:36 -0700

    safe-link

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,56 +1,29 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "list.h"
-/**
- *unique_node_count - counts no. of unique nodes
- *@head:pointer to a head node
- *Return:number of unique nodes,otherwise 0
- */
-size_t unique_node_count(const listint_t *head)
-{
-	listint_t *hare, *tortoise;
-	size_t count = 1;
-
-	if (head == NULL || head->next == NULL)
-	{
-		return (0);
-	}
-	hare = head->next->next;
-	tortoise = head->next;
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				count++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				count++;
-				tortoise = tortoise->next;
-			}
+#include "lists.h"
 
 /**
- *print_listint_safe - prints a listint list
- *@head:pointer to head
- *Return:number of nodes in list
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to head of list
+ * Return: number of nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	unsigned int i = 0;
+	int diff;
 
-	if (head == NULL)
-		exit(98);
+	register short count = 0;
+
 	while (head)
 	{
-		printf("[%p] %d", (void *)head, head->n);
-		i++;
-		head = head->next;
+		diff = head - head->next;
+		count++;
+		printf("[%p] %i\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %i\n", (void *)head->next, head->next->n);
+			break;
+		}
+
 	}
-	return (i);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,9 +1,11 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SUCCESS 1
+#define FAILURE -1
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -18,19 +20,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
- */
-typedef struct listp_s
-{
-	void *p;
-	struct listp_s *next;
-} listp_t;
-
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -47,3 +37,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
+#endif /* LISTS_H */
